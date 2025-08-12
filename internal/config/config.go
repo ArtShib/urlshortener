ab3d81dbdb81e72066cc7d22b97a83ebd8962cee
ArtShib <117906134+ArtShib@users.noreply.github.com>
2025-08-12 13:59:28 +0300

    Update config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -12,7 +12,7 @@ type Config struct {
 } 
 
 func (c *Config) ParseConfig() {
-	flag.StringVar(&c.HttpServer.Port, "a", "8080", "HTTP server startup address")
+	flag.StringVar(&c.HttpServer.Port, "a", ":8080", "HTTP server startup address")
 	flag.StringVar(&c.HttpServer.Host, "b", "localhost", "Address of the resulting shortened URL")
 	flag.Parse()
 }
