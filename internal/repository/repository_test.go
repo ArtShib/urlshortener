5c928fe5072ccdfcab0e08b90f64bf7becf0b1e7
ArtShib <117906134+ArtShib@users.noreply.github.com>
2025-08-12 12:32:04 +0300

    Create repository_test.go

---
+++ internal/repository/repository_test.go
@@ -0,0 +1,43 @@
+package repository
+
+import (
+	"testing"
+
+	"github.com/ArtShib/urlshortener/internal/model"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRepo(t *testing.T) {
+	// type want struct{
+	// 	shortUrl string,
+
+	// }
+	tests := []struct{
+		name string
+		url model.URL
+		want string
+	}{
+		{
+			name:    "test add and get",
+			url: model.URL{
+				LongUrl: "https://www.yandex.com/",
+				ShortCode: "a4d1as",
+			},
+			want: "a4d1as",
+		},
+	}
+	for _, test := range tests{
+		t.Run(test.name, func(t *testing.T){
+			repo := NewRepository()
+			err := repo.Store(&test.url)
+			if err != nil{
+				assert.Errorf(t,err, "Error add")
+			}
+			url, err := repo.FindByShortCode(test.url.ShortCode)
+			if err != nil{
+				assert.Errorf(t,err, "longUrl is not found")
+			}	
+			assert.Equal(t, url.LongUrl, test.url.LongUrl)
+		})
+	}	
+}
