0b266b014de7ade9f7ef11cb20f49ee0351b9fbe
ArtShib <117906134+ArtShib@users.noreply.github.com>
2025-08-12 12:17:38 +0300

    Irer4

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/ArtShib/urlshortener/internal/config"
+	"github.com/ArtShib/urlshortener/internal/repository"
+	"github.com/ArtShib/urlshortener/internal/router"
+	"github.com/ArtShib/urlshortener/internal/service"
+)
+
+func main() {
+	cfg := config.LoadConfig()
+	repo := repository.NewRepository()
+	svc := service.NewUrlService(repo, cfg.HttpServer)
+	router := router.NewRouter(svc)
+
+	err := http.ListenAndServe(cfg.HttpServer.Port, router)	
+	if err != nil{
+		panic(err)
+	}
+}
