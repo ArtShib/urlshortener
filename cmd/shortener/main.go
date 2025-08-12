e37982a64b30d629b346779aeb3dab3592e3bc61
ArtShib <117906134+ArtShib@users.noreply.github.com>
2025-08-12 16:24:28 +0300

    Update main.go

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/ArtShib/urlshortener/internal/config"
 	"github.com/ArtShib/urlshortener/internal/repository"
@@ -15,7 +16,7 @@ func main() {
 	svc := service.NewUrlService(repo, cfg.HttpServer)
 	router := router.NewRouter(svc)
 
-	err := http.ListenAndServe(cfg.HttpServer.Port, router)	
+	err := http.ListenAndServe(strings.Replace(cfg.HttpServer.Port, "::", ":", -1), router)	
 	if err != nil{
 		panic(err)
 	}
