863bf5ae91fb7b8d116430fd0c0f589f81810cd7
ArtShib <117906134+ArtShib@users.noreply.github.com>
2025-08-12 12:58:20 +0300

    Create config.go

---
+++ internal/config/config.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"flag"
+
+	"github.com/ArtShib/urlshortener/internal/model"
+)
+
+type Config struct {
+	HttpServer *model.HttpServerConfig
+	ShortService *model.ShortServiceConfig
+} 
+
+func (c *Config) ParseConfig() {
+	flag.StringVar(&c.HttpServer.Port, "a", "8080", "HTTP server startup address")
+	flag.StringVar(&c.HttpServer.Host, "b", "localhost", "Address of the resulting shortened URL")
+	flag.Parse()
+}
+
+func LoadConfig() *Config {
+	cfg := Config{
+		HttpServer: &model.HttpServerConfig{},
+		ShortService: &model.ShortServiceConfig{},
+	}
+	cfg.ParseConfig()
+	return &cfg
+}
