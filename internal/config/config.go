ec04b1b6c7cb5593d6cc77bc3b40509622fe4f43
ArtShib <117906134+ArtShib@users.noreply.github.com>
2025-08-12 20:38:34 +0300

    Update config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,24 +2,26 @@ package config
 
 import (
 	"flag"
-
+	"strings"
+	
 	"github.com/ArtShib/urlshortener/internal/model"
 )
 
 type Config struct {
-	HttpServer *model.HttpServerConfig
+	HTTPServer *model.HTTPServerConfig
 	ShortService *model.ShortServiceConfig
 } 
 
 func (c *Config) ParseConfig() {
-	flag.StringVar(&c.HttpServer.Port, "a", ":8080", "HTTP server startup address")
-	flag.StringVar(&c.HttpServer.Host, "b", "localhost", "Address of the resulting shortened URL")
+	flag.StringVar(&c.HTTPServer.Port, "a", ":8080", "HTTP server startup address")
+	flag.StringVar(&c.HTTPServer.Host, "b", "localhost", "Address of the resulting shortened URL")
 	flag.Parse()
+	c.HTTPServer.Port = strings.Replace(c.HTTPServer.Port, "::", ":", -1)
 }
 
 func LoadConfig() *Config {
 	cfg := Config{
-		HttpServer: &model.HttpServerConfig{},
+		HTTPServer: &model.HTTPServerConfig{},
 		ShortService: &model.ShortServiceConfig{},
 	}
 	cfg.ParseConfig()
