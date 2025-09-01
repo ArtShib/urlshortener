b467d5f3a32e6044419a5f4a9beb9c448bd99ba0
ArtShib <117906134+ArtShib@users.noreply.github.com>
2025-09-01 16:11:09 +0300

    Create gzip.go

---
+++ internal/handler/middleware/gzip.go
@@ -0,0 +1,53 @@
+package middleware
+
+import (
+	"compress/gzip"
+	"net/http"
+	"strings"
+)
+
+func GzipMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		contentType := r.Header.Get("Content-Type")
+
+		if r.Header.Get("Content-Encoding") == "gzip" {
+			gzipReader, err := gzip.NewReader(r.Body)
+			if err != nil {
+				http.Error(w, "Invalid gzip bady", http.StatusBadRequest)
+				return
+			}
+			defer gzipReader.Close()
+			r.Body = gzipReader
+		}
+		
+		if strings.HasPrefix(contentType,"application/json") || strings.HasPrefix(contentType, "text/html") {
+			
+			if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+				next.ServeHTTP(w, r)
+				return
+			}
+				
+			w.Header().Set("Content-Encoding", "gzip")
+			gzipWriter := gzip.NewWriter(w)
+			defer gzipWriter.Close()
+
+			gzw := gzipResponseWriter{ResponseWriter: w, Writer: gzipWriter}
+
+			next.ServeHTTP(gzw, r)
+		}else{
+			next.ServeHTTP(w, r)	
+			return
+		}
+	})
+}
+	
+
+type gzipResponseWriter struct {
+	http.ResponseWriter
+	Writer *gzip.Writer
+}
+
+func (w gzipResponseWriter) Write(b []byte) (int, error) {
+	return w.Writer.Write(b)
+}
