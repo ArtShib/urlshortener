d20546ac43fd8e26c9cb57683b75b770e4dd3aaf
ArtShib <117906134+ArtShib@users.noreply.github.com>
2025-09-01 15:58:58 +0300

    Update main.go

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,20 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/ArtShib/urlshortener/internal/config"
 	"github.com/ArtShib/urlshortener/internal/handler"
+	"github.com/ArtShib/urlshortener/internal/lib/logger"
 	"github.com/ArtShib/urlshortener/internal/repository"
 	"github.com/ArtShib/urlshortener/internal/service"
 )
 
 func main() {
 	cfg := config.MustLoadConfig()
-	repo := repository.NewRepository()
+	repo, _ := repository.NewRepository(cfg.RepoConfig.FileStoragePath)
+	defer func(){
+		if err := repo.SavingRepository("/Users/shibakin-av/IdeaProjects/go/urlshortener/storage/storage3.json"); err != nil {
+			fmt.Println(err)
+		}
+	}()
+	
 	svc := service.NewURLService(repo, cfg.ShortService)
-	router := handler.NewRouter(svc)
+	router := handler.NewRouter(svc, logger.NewLogger())
 
-	log.Fatal(http.ListenAndServe(cfg.HTTPServer.Port, router))	
+	go func() {
+		log.Fatal(http.ListenAndServe(cfg.HTTPServer.ServerAddress, router))	
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT)
+	<-sigChan
 }
