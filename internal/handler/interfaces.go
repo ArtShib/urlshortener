e21dbd700b0c9fa08d1b3bf3f76f2aaeecb14b93
ArtShib <117906134+ArtShib@users.noreply.github.com>
2025-09-01 17:55:40 +0300

    Update interfaces.go

--- internal/handler/interfaces.go
+++ internal/handler/interfaces.go
@@ -6,5 +6,5 @@ import "github.com/ArtShib/urlshortener/internal/model"
 type URLService interface {
 	Shorten(url string) (string, error)
 	GetID(shortCode string) (string, error)
-	ShortenJson(url string) (*model.ResponseShortener, error)
+	ShortenJSON(url string) (*model.ResponseShortener, error)
 }
