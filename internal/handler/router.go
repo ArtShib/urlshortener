2f33b0053a0cfb016d56c349ff5c567abfaf3346
ArtShib <117906134+ArtShib@users.noreply.github.com>
2025-09-01 16:08:55 +0300

    Update router.go

--- internal/handler/router.go
+++ internal/handler/router.go
@@ -1,21 +1,29 @@
 package handler
 
 import (
+	"log/slog"
 	"net/http"
 
+	customMiddleware "github.com/ArtShib/urlshortener/internal/handler/middleware"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 
-func NewRouter(svc URLService) http.Handler {
+func NewRouter(svc URLService, log *slog.Logger) http.Handler {
 	//mux := http.NewServeMux()
 	mux := chi.NewRouter()
-	handler := NewURLHandler(svc)
 
-	// mux.HandleFunc(`/`, handler.Shorten)
-	// mux.HandleFunc(`/{shortCode}`, handler.GetID)
+	mux.Use(middleware.RequestID)
+	mux.Use(middleware.Logger)
+	mux.Use(middleware.Recoverer)
+	mux.Use(customMiddleware.New(log))
+	mux.Use(customMiddleware.GzipMiddleware)
+
+	handler := NewURLHandler(svc)
 	mux.Post("/", handler.Shorten)
-	mux.Get("/{shortCode}", handler.GetID)
+	mux.Post("/api/shorten", handler.ShortenJson)
+	mux.Get("/{shortCode}",  handler.GetID)
 	
 	return mux
 }
