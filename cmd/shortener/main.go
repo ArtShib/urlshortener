96ccdeaa6baf389d39691739d597afbcfd0359a2
ArtShib <117906134+ArtShib@users.noreply.github.com>
2025-09-02 16:51:13 +0300

    Update main.go

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,49 +1,23 @@
 package main
 
 import (
-	"context"
-	"log"
-	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
+	"github.com/ArtShib/urlshortener/internal/app"
 	"github.com/ArtShib/urlshortener/internal/config"
-	"github.com/ArtShib/urlshortener/internal/handler"
-	"github.com/ArtShib/urlshortener/internal/lib/logger"
-	"github.com/ArtShib/urlshortener/internal/repository"
-	"github.com/ArtShib/urlshortener/internal/service"
 )
 
 func main() {
 	cfg := config.MustLoadConfig()
+	app, _ := app.NewApp(cfg)
 	
-	logg := logger.NewLogger()
-	repo, _ := repository.NewRepository(cfg.RepoConfig.FileStoragePath)
-
-	svc := service.NewURLService(repo, cfg.ShortService)
-	router := handler.NewRouter(svc, logg)
-	
-	server := &http.Server{
-		Addr: cfg.HTTPServer.ServerAddress,
-		Handler: router,
-	}
-
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP, syscall.SIGQUIT)
+	go app.Run()
 
-	
-	go func() {
-		log.Fatal(server.ListenAndServe())
-	}()
-	
 	<-quit
 	
-	ctx, cansel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cansel()
-	
-	repo.SavingRepository(cfg.RepoConfig.FileStoragePath)
-	
-	server.Shutdown(ctx)
+	app.Stop()
 }
