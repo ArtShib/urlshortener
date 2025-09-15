37831e890f96770ca1a27544f97f245c19bf1c63
ArtShib <117906134+ArtShib@users.noreply.github.com>
2025-09-15 22:22:15 +0300

    Update interfaces.go

--- internal/handler/interfaces.go
+++ internal/handler/interfaces.go
@@ -1,16 +1,10 @@
 package handler
 
-import (
-	"context"
-
-	"github.com/ArtShib/urlshortener/internal/model"
-)
+import "github.com/ArtShib/urlshortener/internal/model"
 
 
 type URLService interface {
 	Shorten(url string) (string, error)
 	GetID(shortCode string) (string, error)
 	ShortenJSON(url string) (*model.ResponseShortener, error)
-	Ping(ctx context.Context) error
-	ShortenJSONBatch(urls model.RequestShortenerBatchArray) (model.ResponseShortenerBatchArray, error)
 }
