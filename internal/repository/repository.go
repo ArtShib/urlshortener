c51a7e723895525fb637869283a5f407b0152e18
ArtShib <117906134+ArtShib@users.noreply.github.com>
2025-09-15 22:11:22 +0300

    Update repository.go

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,16 +1,30 @@
 package repository
 
 import (
+	"context"
+	"log"
+
 	"github.com/ArtShib/urlshortener/internal/model"
 	"github.com/ArtShib/urlshortener/internal/repository/memory"
+	"github.com/ArtShib/urlshortener/internal/repository/postgres"
 )
 
+
+
 type URLRepository interface {
-	Store(url *model.URL) error
-	FindByShortCode(uuid string) (*model.URL, error)
-	SavingRepository(fileName string) error 
+	Save(ctx context.Context, url *model.URL) (*model.URL, error)
+	Get(ctx context.Context, uuid string) (*model.URL, error)
+	Close() error
+	Ping(context.Context) error
 }
-func NewRepository(fileName string) (URLRepository, error) {
-	repo, err := memory.NewMemoryRepository(fileName)
-	return repo, err
+
+func NewRepository(ctx context.Context, repoType string, dsnORpath string) (URLRepository, error) {
+	switch repoType {
+	case "db":
+		return postgres.NewPostgresRepository(ctx, dsnORpath), nil
+	case "file":
+		return memory.NewMemoryRepository(ctx, dsnORpath)
+	}
+	log.Fatal("not loaded repo")
+	return nil, nil
 }
