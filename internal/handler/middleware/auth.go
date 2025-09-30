6c596db7620d05f1fe5ef01347cc9e87fe73f13b
artem <shibakin@bk.ru>
2025-09-30 18:46:08 +0300

    GetJSONBatch

--- internal/handler/middleware/auth.go
+++ internal/handler/middleware/auth.go
@@ -5,11 +5,46 @@ import (
 	"net/http"
 
 	"github.com/ArtShib/urlshortener/internal/lib/auth"
+	"github.com/go-chi/chi/middleware"
 )
 
 func Auth(auth *auth.AuthService) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		//return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		//	var userID string
+		//	type contextKey string
+		//	const userIDKey contextKey = "userID"
+		//	c, err := r.Cookie("token")
+		//	if err != nil {
+		//		userID, err = auth.GenerateUserID()
+		//		if err != nil {
+		//			http.Error(w, err.Error(), http.StatusInternalServerError)
+		//			return
+		//		}
+		//	} else if !auth.ValidateToken(c.Value) {
+		//		userID, err = auth.GenerateUserID()
+		//		if err != nil {
+		//			http.Error(w, err.Error(), http.StatusInternalServerError)
+		//			return
+		//		}
+		//	} else {
+		//		userID = auth.GetUserID(c.Value)
+		//		if userID == "" {
+		//			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		//			return
+		//		}
+		//	}
+		//	token := auth.CreateToken(userID)
+		//
+		//	http.SetCookie(w, &http.Cookie{
+		//		Name:  "User",
+		//		Value: token,
+		//	})
+		//	ctx := context.WithValue(r.Context(), userIDKey, userID)
+		//	r = r.WithContext(ctx)
+		//	next.ServeHTTP(w, r)
+		//})
+		fn := func(w http.ResponseWriter, r *http.Request) {
 			var userID string
 			type contextKey string
 			const userIDKey contextKey = "userID"
@@ -41,7 +76,10 @@ func Auth(auth *auth.AuthService) func(next http.Handler) http.Handler {
 			})
 			ctx := context.WithValue(r.Context(), userIDKey, userID)
 			r = r.WithContext(ctx)
-			next.ServeHTTP(w, r)
-		})
+
+			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+			next.ServeHTTP(ww, r)
+		}
+		return http.HandlerFunc(fn)
 	}
 }
