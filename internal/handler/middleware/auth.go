70738517021ab1df935d96716d11eb9f32d862c4
artem <shibakin@bk.ru>
2025-09-30 18:50:04 +0300

    GetJSONBatch

--- internal/handler/middleware/auth.go
+++ internal/handler/middleware/auth.go
@@ -5,46 +5,11 @@ import (
 	"net/http"
 
 	"github.com/ArtShib/urlshortener/internal/lib/auth"
-	"github.com/go-chi/chi/middleware"
 )
 
 func Auth(auth *auth.AuthService) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
-		//return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		//	var userID string
-		//	type contextKey string
-		//	const userIDKey contextKey = "userID"
-		//	c, err := r.Cookie("token")
-		//	if err != nil {
-		//		userID, err = auth.GenerateUserID()
-		//		if err != nil {
-		//			http.Error(w, err.Error(), http.StatusInternalServerError)
-		//			return
-		//		}
-		//	} else if !auth.ValidateToken(c.Value) {
-		//		userID, err = auth.GenerateUserID()
-		//		if err != nil {
-		//			http.Error(w, err.Error(), http.StatusInternalServerError)
-		//			return
-		//		}
-		//	} else {
-		//		userID = auth.GetUserID(c.Value)
-		//		if userID == "" {
-		//			http.Error(w, "Unauthorized", http.StatusUnauthorized)
-		//			return
-		//		}
-		//	}
-		//	token := auth.CreateToken(userID)
-		//
-		//	http.SetCookie(w, &http.Cookie{
-		//		Name:  "User",
-		//		Value: token,
-		//	})
-		//	ctx := context.WithValue(r.Context(), userIDKey, userID)
-		//	r = r.WithContext(ctx)
-		//	next.ServeHTTP(w, r)
-		//})
-		fn := func(w http.ResponseWriter, r *http.Request) {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			var userID string
 			type contextKey string
 			const userIDKey contextKey = "userID"
@@ -75,11 +40,8 @@ func Auth(auth *auth.AuthService) func(next http.Handler) http.Handler {
 				Value: token,
 			})
 			ctx := context.WithValue(r.Context(), userIDKey, userID)
-			r = r.WithContext(ctx)
-
-			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
-			next.ServeHTTP(ww, r)
-		}
-		return http.HandlerFunc(fn)
+			//r = r.WithContext(ctx)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
 	}
 }
