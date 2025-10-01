d79f5d011520925a86961caaf6c386369f6d43ed
artem <shibakin@bk.ru>
2025-10-01 11:12:48 +0300

    GetJSONBatch

--- internal/handler/middleware/auth.go
+++ internal/handler/middleware/auth.go
@@ -13,27 +13,45 @@ func Auth(auth *auth.AuthService) func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			var userID, token string
 
-			c, err := r.Cookie("User")
+			_, err := r.Cookie("User")
 			if err != nil {
 				userID, err = auth.GenerateUserID()
 				token = auth.CreateToken(userID)
+				http.SetCookie(w, &http.Cookie{
+					Name:  "User",
+					Value: token,
+				})
 				if err != nil {
 					http.Error(w, err.Error(), http.StatusInternalServerError)
 					return
 				}
-			} else {
-				userID = auth.GetUserID(c.Value)
-				token = c.Value
-				if userID == "" {
-					http.Error(w, "Unauthorized", http.StatusUnauthorized)
-					return
-				}
 			}
-
-			http.SetCookie(w, &http.Cookie{
-				Name:  "User",
-				Value: token,
-			})
+			//else if !auth.ValidateToken(cookie.Value) {
+			//	userID, err = auth.GenerateUserID()
+			//	token = auth.CreateToken(userID)
+			//	if err != nil {
+			//		http.Error(w, err.Error(), http.StatusInternalServerError)
+			//		return
+			//	}
+			//} else {
+			//	userID = auth.GetUserID(cookie.Value)
+			//	token = cookie.Value
+			//	if userID == "" {
+			//		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			//		return
+			//	}
+			//}
+			//log.Fatal("EEEEEEEEEEEEEEEEEEEE", cookie.Value)
+			//userID = auth.GetUserID(cookie.Value)
+			//if userID == "" {
+			//	http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			//	return
+			//}
+			//token = cookie.Value
+			//http.SetCookie(w, &http.Cookie{
+			//	Name:  "User",
+			//	Value: token,
+			//})
 			ctx := context.WithValue(r.Context(), model.UserIDKey, userID)
 			//r = r.WithContext(ctx)
 			next.ServeHTTP(w, r.WithContext(ctx))
